bc5a3026a5ea7a1cd7335a7650a249b3b9e177d0
Jan Nash <jan@resmio.com>
2017-08-07 16:37:57 +0200

    Create Xcode-project

---
+++ SignificantSpices/SignificantSpices.h
@@ -0,0 +1,19 @@
+//
+//  SignificantSpices.h
+//  SignificantSpices
+//
+//  Created by Jan Nash on 8/7/17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SignificantSpices.
+FOUNDATION_EXPORT double SignificantSpicesVersionNumber;
+
+//! Project version string for SignificantSpices.
+FOUNDATION_EXPORT const unsigned char SignificantSpicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignificantSpices/PublicHeader.h>
+
+
