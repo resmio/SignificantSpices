475435350d46e2a47da35df90898a105ace7d552
Jan Nash <jan@resmio.com>
2017-08-08 16:24:55 +0200

    Add Bool in-place assignment operators

--- SignificantSpices/Sources/Swift/CustomOperators/CustomOperators.swift
+++ SignificantSpices/Sources/Swift/CustomOperators/CustomOperators.swift
@@ -6,3 +6,17 @@
 //  Copyright © 2017 resmio. All rights reserved.
 //
 
+
+// MARK: - - Bool In-Place Assignment Operators
+// MARK: in-place &&
+infix operator &&= : AssignmentPrecedence
+public func &&= (lhs: inout Bool, rhs: Bool) {
+    lhs = lhs && rhs
+}
+
+// MARK: in-place ||
+infix operator ||= : AssignmentPrecedence
+public func ||= (lhs: inout Bool, rhs: Bool) {
+    lhs = lhs || rhs
+}
+
