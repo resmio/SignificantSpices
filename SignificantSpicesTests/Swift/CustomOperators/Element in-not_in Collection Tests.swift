7f421dac17d863ef5540b5262babd8ec417b2eb9
Jan Nash <jan@resmio.com>
2017-08-08 18:57:59 +0200

    Finish adding tests for <> and ><

--- SignificantSpicesTests/Swift/CustomOperators/Element in-not_in Collection Tests.swift
+++ SignificantSpicesTests/Swift/CustomOperators/Element in-not_in Collection Tests.swift
@@ -11,48 +11,73 @@ import SignificantSpices.Swift
 
 
 class NilInOrNotInArrayTests: XCTestCase {
-    func testNilInNonEquatableNSObjArray() {
-        let a: [_NSObjFoo] = [_NSObjFoo(), _NSObjFoo(), _NSObjFoo()]
+    func testNilIn_NERT_Array() {
+        let a: [_NERT] = [_NERT(), _NERT(), _NERT()]
+        
         let nilInA: Bool = nil <> a
         XCTAssertFalse(nilInA)
-    }
-    
-    func testNilNotInNonEquatableNSObjArray() {
-        let a: [_NSObjFoo] = [_NSObjFoo(), _NSObjFoo(), _NSObjFoo()]
+        
         let nilNotInA: Bool = nil >< a
         XCTAssertTrue(nilNotInA)
     }
     
-    func testNilInEquatableNonNSObjArray() {
-        let a: [Int] = [1, 2, 3]
+    func testNilNotIn_NERT_Array() {
+        let a: [_NERT] = [_NERT(), _NERT(), _NERT()]
+        
+        let nilNotInA: Bool = nil >< a
+        XCTAssertTrue(nilNotInA)
+        
         let nilInA: Bool = nil <> a
         XCTAssertFalse(nilInA)
     }
     
-    func testNilNotInEquatableNonNSObjArray() {
-        let a: [Int] = [1, 2, 3]
+    func testNilIn_ENRT_Array() {
+        let a: [_ENRT] = [1, 2, 3]
+        
+        let nilInA: Bool = nil <> a
+        XCTAssertFalse(nilInA)
+        
         let nilNotInA: Bool = nil >< a
         XCTAssertTrue(nilNotInA)
     }
     
-    func testNilInEquatableNSObjArray() {
-        let a: [_EqNSObjFoo] = [_EqNSObjFoo(1), _EqNSObjFoo(2), _EqNSObjFoo(3)]
+    func testNilNotIn_ENRT_Array() {
+        let a: [_ENRT] = [1, 2, 3]
+        
+        let nilNotInA: Bool = nil >< a
+        XCTAssertTrue(nilNotInA)
+        
         let nilInA: Bool = nil <> a
         XCTAssertFalse(nilInA)
     }
     
-    func testNilNotInEquatableNSObjArray() {
-        let a: [_EqNSObjFoo] = [_EqNSObjFoo(1), _EqNSObjFoo(2), _EqNSObjFoo(3)]
+    func testNilIn_ERT_Array() {
+        let a: [_ERT] = [_ERT(1), _ERT(2), _ERT(3)]
+        
+        let nilInA: Bool = nil <> a
+        XCTAssertFalse(nilInA)
+        
         let nilNotInA: Bool = nil >< a
         XCTAssertTrue(nilNotInA)
+    }
+    
+    func testNilNotIn_ERT_Array() {
+        let a: [_ERT] = [_ERT(1), _ERT(2), _ERT(3)]
+        
+        let nilNotInA: Bool = nil >< a
+        XCTAssertTrue(nilNotInA)
+        
+        let nilInA: Bool = nil <> a
+        XCTAssertFalse(nilInA)
     }
 }
 
-class NonEquatableInOrNotInArrayTests: XCTestCase {
-    func testNonEquatableInArray() {
-        let a: _Foo = _Foo()
-        let b: _Foo = a
-        let c: [_Foo] = [a, _Foo(), _Foo()]
+
+class NERT_InOrNotInArrayTests: XCTestCase {
+    func test_NERT_InArray() {
+        let a: _NERT = _NERT()
+        let b: _NERT = a
+        let c: [_NERT] = [a, _NERT(), _NERT()]
         
         let aInC: Bool = a <> c
         XCTAssertTrue(aInC)
@@ -67,10 +92,10 @@ class NonEquatableInOrNotInArrayTests: XCTestCase {
         XCTAssertFalse(bNotInC)
     }
     
-    func testNonEquatableNotInArray() {
-        let a: _Foo = _Foo()
-        let b: _Foo = a
-        let c: [_Foo] = [_Foo(), _Foo(), _Foo()]
+    func test_NERT_NotInArray() {
+        let a: _NERT = _NERT()
+        let b: _NERT = a
+        let c: [_NERT] = [_NERT(), _NERT(), _NERT()]
         
         let aNotInC: Bool = a >< c
         XCTAssertTrue(aNotInC)
@@ -86,41 +111,157 @@ class NonEquatableInOrNotInArrayTests: XCTestCase {
     }
 }
 
-class EquatableElementInOrNotInArrayTests: XCTestCase {
+
+class ENRT_InOrNotInArrayTests: XCTestCase {
+    func test_ENRT_InArray() {
+        let a: _ENRT = 1
+        let b: _ENRT = 1
+        let c: [_ENRT] = [a, 2, 3]
+        
+        let aInC: Bool = a <> c
+        XCTAssertTrue(aInC)
+        
+        let bInC: Bool = b <> c
+        XCTAssertTrue(bInC)
+        
+        let aNotInC: Bool = a >< c
+        XCTAssertFalse(aNotInC)
+        
+        let bNotInC: Bool = b >< c
+        XCTAssertFalse(bNotInC)
+    }
     
+    func test_ENRT_NotInArray() {
+        let a: _ENRT = 1
+        let b: _ENRT = 1
+        let c: [_ENRT] = [2, 3, 4]
+        
+        let aNotInC: Bool = a >< c
+        XCTAssertTrue(aNotInC)
+        
+        let bNotInC: Bool = b >< c
+        XCTAssertTrue(bNotInC)
+        
+        let aInC: Bool = a <> c
+        XCTAssertFalse(aInC)
+        
+        let bInC: Bool = b <> c
+        XCTAssertFalse(bInC)
+    }
 }
+
+
+class ERT_InOrNotInArrayTests: XCTestCase {
+    func test_ERT_InArray() {
+        let a: _ERT = _ERT(1)
+        let b: _ERT = _ERT(1)
+        let c: [_ERT] = [a, _ERT(2), _ERT(3)]
+        
+        let aInC: Bool = a <> c
+        XCTAssertTrue(aInC)
+        
+        let bInC: Bool = b <> c
+        XCTAssertTrue(bInC)
+        
+        let aNotInC: Bool = a >< c
+        XCTAssertFalse(aNotInC)
+        
+        let bNotInC: Bool = b >< c
+        XCTAssertFalse(bNotInC)
+    }
+    
+    func test_ERT_NotInArray() {
+        let a: _ERT = _ERT(1)
+        let b: _ERT = _ERT(1)
+        let c: [_ERT] = [_ERT(2), _ERT(3), _ERT(4)]
+        
+        let aNotInC: Bool = a >< c
+        XCTAssertTrue(aNotInC)
+        
+        let bNotInC: Bool = b >< c
+        XCTAssertTrue(bNotInC)
+        
+        let aInC: Bool = a <> c
+        XCTAssertFalse(aInC)
+        
+        let bInC: Bool = b <> c
+        XCTAssertFalse(bInC)
+    }
+}
+
 
 class NilInOrNotInSetTests: XCTestCase {
     func testNilInSet() {
         let a: Set<Int> = Set([1, 2, 3])
-        let b: Bool = nil <> a
-        XCTAssertFalse(b)
+        
+        let nilInA: Bool = nil <> a
+        XCTAssertFalse(nilInA)
+        
+        let nilNotInA: Bool = nil >< a
+        XCTAssertTrue(nilNotInA)
     }
     
-    func testNilNotInSet() {
+    func testNilNotIn_HNRT_Set() {
         let a: Set<Int> = Set([1, 2, 3])
-        let b: Bool = nil >< a
-        XCTAssertTrue(b)
+        
+        let nilNotInA: Bool = nil >< a
+        XCTAssertTrue(nilNotInA)
+        
+        let nilInA: Bool = nil <> a
+        XCTAssertFalse(nilInA)
     }
 }
 
-class HashableElementInOrNotInSetTests: XCTestCase {
+
+class Int_InOrNotInSetTests: XCTestCase {
+    func testIntInSet() {
+        let a: Int = 1
+        let b: Int = a
+        
+        let c: Set<Int> = Set([a, 2, 3])
+        
+        let aInC: Bool = a <> c
+        XCTAssertTrue(aInC)
+        
+        let bInC: Bool = b <> c
+        XCTAssertTrue(bInC)
+        
+        let aNotInC: Bool = a >< c
+        XCTAssertFalse(aNotInC)
+        
+        let bNotInC: Bool = b >< c
+        XCTAssertFalse(bNotInC)
+    }
     
+    func testIntNotInSet() {
+        let a: Int = 1
+        let b: Int = a
+        
+        let c: Set<Int> = Set([2, 3, 4])
+        
+        let aInC: Bool = a <> c
+        XCTAssertFalse(aInC)
+        
+        let bInC: Bool = b <> c
+        XCTAssertFalse(bInC)
+        
+        let aNotInC: Bool = a >< c
+        XCTAssertTrue(aNotInC)
+        
+        let bNotInC: Bool = b >< c
+        XCTAssertTrue(bNotInC)
+    }
 }
 
 
 // MARK: // Private
-// MARK: Private Helper Classes
-private class _Foo {}
-private class _NSObjFoo: NSObject {}
-private class _EqNSObjFoo: NSObject {
-    init(_ value: Int) {
-        self.value = value
-    }
-    
+// MARK: Helper Types
+private typealias _ENRT = Int /*Equatable Non-Reference Type*/
+private class _NERT /*Non Equatable Reference Type*/ {}
+private class _ERT: Equatable /*Equatable Reference Type*/ {
+    init(_ value: Int) { self.value = value }
     private(set) var value: Int = 0
-    
-    static func ==(_ lhs: _EqNSObjFoo, _ rhs: _EqNSObjFoo) -> Bool {
+    static func ==(_ lhs: _ERT, _ rhs: _ERT) -> Bool {
         return lhs.value == rhs.value
     }
 }
