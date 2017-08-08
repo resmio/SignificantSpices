8b90cc510cda6b2e60cee268addbdd9929a04e6a
Jan Nash <jan@resmio.com>
2017-08-08 16:25:12 +0200

    Add Set combination operators

--- SignificantSpices/Sources/Swift/CustomOperators/CustomOperators.swift
+++ SignificantSpices/Sources/Swift/CustomOperators/CustomOperators.swift
@@ -20,3 +20,29 @@ public func ||= (lhs: inout Bool, rhs: Bool) {
     lhs = lhs || rhs
 }
 
+
+// MARK: - - Set Combination Operators
+// MARK: Intersection
+infix operator & : ComparisonPrecedence
+public func & <T: Hashable>(l: Set<T>, r: Set<T>) -> Set<T> {
+    return l.intersection(r)
+}
+
+// MARK: Union
+infix operator | : ComparisonPrecedence
+public func | <T: Hashable>(l: Set<T>, r: Set<T>) -> Set<T> {
+    return l.union(r)
+}
+
+// MARK: Substraction
+infix operator - : ComparisonPrecedence
+public func - <T: Hashable>(l: Set<T>, r: Set<T>) -> Set<T> {
+    return l.subtracting(r)
+}
+
+// MARK: Symmetric Difference
+infix operator ^ : ComparisonPrecedence
+public func ^ <T: Hashable>(l: Set<T>, r: Set<T>) -> Set<T> {
+    return l.symmetricDifference(r)
+}
+
