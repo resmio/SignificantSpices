4a5b2f20208b587bd79551049d6951e32251980f
Jan Nash <jan@resmio.com>
2017-08-09 17:22:12 +0200

    Add Collection (SafeSubscript)

---
+++ SignificantSpices/Sources/Swift/Extensions/Collection/Collection (SafeSubscript).swift
@@ -0,0 +1,14 @@
+//
+//  Collection (SafeSubscript).swift
+//  SignificantSpices
+//
+//  Created by Jan Nash on 8/9/17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+
+extension Collection where Indices.Iterator.Element == Index {
+    public subscript (safe index: Index) -> Generator.Element? {
+        return self.indices.contains(index) ? self[index] : nil
+    }
+}
