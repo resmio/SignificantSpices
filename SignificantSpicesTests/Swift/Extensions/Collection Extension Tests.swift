aaa56a79f7e710478a8997fddbbf2cb097550b6d
Jan Nash <jan@resmio.com>
2017-08-09 17:28:27 +0200

    Add tests for safeSubscript

---
+++ SignificantSpicesTests/Swift/Extensions/Collection Extension Tests.swift
@@ -0,0 +1,21 @@
+//
+//  Collection Tests.swift
+//  SignificantSpices
+//
+//  Created by Jan Nash on 8/9/17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+import XCTest
+import SignificantSpices.Swift
+
+
+class CollectionExtensionTests: XCTestCase {
+    func testSafeSubscriptReturnsNilOnIndexOutOfBounds() {
+        XCTAssertNil([1, 2, 3, 4][safe: 4])
+    }
+    
+    func testSafeSubscriptReturnsElementOnIndexInBounds() {
+        XCTAssertEqual([1, 2, 3, 4][safe: 3], 4)
+    }
+}
