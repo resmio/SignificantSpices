3e31602db09baca29f9bd869902112bb3ebc5142
Jan Nash <jan@resmio.com>
2017-08-09 17:42:04 +0200

    Add Optional isNilOrEmpty extensions and respective tests

---
+++ SignificantSpices/Sources/Swift/Extensions/Optional/Optional (IsNilOrEmpty).swift
@@ -0,0 +1,23 @@
+//
+//  Optional (IsNilOrEmpty).swift
+//  SignificantSpices
+//
+//  Created by Jan Nash on 8/9/17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+
+// MARK: Optional<String>
+extension Optional where Wrapped == String {
+    public var isNilOrEmpty: Bool {
+        return self?.isEmpty ?? true
+    }
+}
+
+
+// MARK: Optional<Collection>
+extension Optional where Wrapped: Collection {
+    public var isNilOrEmpty: Bool {
+        return self?.isEmpty ?? true
+    }
+}
---
+++ SignificantSpicesTests/Swift/Extensions/Optional Extension Tests.swift
@@ -0,0 +1,43 @@
+//
+//  Optional Extension Tests.swift
+//  SignificantSpices
+//
+//  Created by Jan Nash on 8/9/17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+import XCTest
+import SignificantSpices.Swift
+
+
+class OptionalExtensionTests: XCTestCase {
+    func testNilCollectionIsNilOrEmpty() {
+        let a: [Int]? = nil
+        XCTAssertTrue(a.isNilOrEmpty)
+    }
+    
+    func testEmptyCollectionIsNilOrEmpty() {
+        let a: [Int]? = []
+        XCTAssertTrue(a.isNilOrEmpty)
+    }
+    
+    func testNonEmptyCollectionIsNilOrEmpty() {
+        let a: [Int]? = [1]
+        XCTAssertFalse(a.isNilOrEmpty)
+    }
+    
+    func testNilStringIsNilOrEmpty() {
+        let a: String? = nil
+        XCTAssertTrue(a.isNilOrEmpty)
+    }
+    
+    func testEmptyStringIsNilOrEmpty() {
+        let a: String? = ""
+        XCTAssertTrue(a.isNilOrEmpty)
+    }
+    
+    func testNonEmptyStringIsNilOrEmpty() {
+        let a: String? = "42"
+        XCTAssertFalse(a.isNilOrEmpty)
+    }
+}
