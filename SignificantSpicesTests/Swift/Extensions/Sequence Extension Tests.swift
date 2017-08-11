2e724c475708d0c1de0af773de7a7ff3329f3f22
Jan Nash <jan@resmio.com>
2017-08-11 17:56:57 +0200

    Create test-file for Sequence extension

---
+++ SignificantSpicesTests/Swift/Extensions/Sequence Extension Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Sequence Extension Tests.swift
+//  SignificantSpices
+//
+//  Created by Jan Nash on 8/11/17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+import XCTest
+import SignificantSpices.Swift
+
+
+class SequenceGroupedByTests: XCTestCase {
+    func testSequenceGroupedBy() {
+        
+    }
+}
