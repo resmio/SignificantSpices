aaf4b25324568fa904d6b0c56d8651dabcb9c791
Jan Nash <jan@resmio.com>
2017-08-14 15:32:13 +0200

    Add test for closure negation operator

---
+++ SignificantSpicesTests/Swift/CustomOperators/(T) -> Bool Negation.swift
@@ -0,0 +1,19 @@
+//
+//  (T) -> Bool Negation.swift
+//  SignificantSpices
+//
+//  Created by Jan Nash on 8/14/17.
+//  Copyright © 2017 resmio. All rights reserved.
+//
+
+import XCTest
+import SignificantSpices.Swift
+
+
+class BoolClosureNegationTests: XCTestCase {
+    func testBoolClosureNegation() {
+        let closure: (Int) -> Bool = { $0 == 5 }
+        let negatedClosure: (Int) -> Bool = !closure
+        XCTAssertFalse(negatedClosure(5))
+    }
+}
