8bbb85b633edeb0e1d6adb7d1252328c94e12167
Jan Nash <jan@resmio.com>
2017-08-14 16:30:22 +0200

    Document Sequence.none() to avoid confusion about vacuous truth

--- SignificantSpices/Sources/Swift/Extensions/Sequence/Sequence (none).swift
+++ SignificantSpices/Sources/Swift/Extensions/Sequence/Sequence (none).swift
@@ -9,6 +9,20 @@
 
 // MARK: // Public
 extension Sequence {
+    /// Returns a Boolean value indicating whether the sequence contains any
+    /// element that does fulfill the given condition.
+    ///
+    /// Calling the method on an empty Sequence will always return `true`.
+    /// See also: [Vacuous Truth (Wikipedia)](https://en.wikipedia.org/wiki/Vacuous_truth).
+    ///
+    /// - Parameter condition:
+    ///     A closure that takes an element of the sequence as its argument
+    ///     and returns a Boolean value that indicates whether the passed
+    ///     element represents a match.
+    /// - Returns:
+    ///     `false` if the Sequence contains ANY element that DOES fulfill
+    ///     the condition; otherwise `true`.
+    ///
     public func none(fulfills condition: (Iterator.Element) -> Bool) -> Bool {
         return !self.contains(where: condition)
     }
