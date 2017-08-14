851f2595154c6a306e2270b37656a79a0af33fa6
Jan Nash <jan@resmio.com>
2017-08-14 16:16:27 +0200

    Document Sequence.all() to avoid confusion about empty sequences

--- SignificantSpices/Sources/Swift/Extensions/Sequence/Sequence (all).swift
+++ SignificantSpices/Sources/Swift/Extensions/Sequence/Sequence (all).swift
@@ -10,6 +10,23 @@
 // MARK: // Public
 // MARK: forAll (condition)
 extension Sequence {
+    /// Returns a Boolean value indicating whether the sequence contains any
+    /// element that does not fulfill the given condition.
+    ///
+    /// The behaviour of this method is defined the same way as in python's 
+    /// `all()`, which means that calling the method on an empty Sequence 
+    /// will always return `true`.
+    ///
+    /// See also: [Vacuous Truth (Wikipedia)](https://en.wikipedia.org/wiki/Vacuous_truth).
+    ///
+    /// - Parameter condition: 
+    ///     A closure that takes an element of the sequence as its argument 
+    ///     and returns a Boolean value that indicates whether the passed 
+    ///     element represents a match.
+    /// - Returns: 
+    ///     `false` if the Sequence contains ANY element that does NOT fulfill 
+    ///     the condition; otherwise `true`.
+    ///
     public func all(fulfill condition: ((Iterator.Element) -> Bool)) -> Bool {
         return !self.contains(where: { !condition($0) })
     }
