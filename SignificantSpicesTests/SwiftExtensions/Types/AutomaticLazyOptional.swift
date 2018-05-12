139412621e52254576bfd2afb297d7fb3d51afad
Jan Nash <jan@resmio.com>
2018-05-12 22:16:52 +0200

    Use ALO typealias in tests

--- SignificantSpicesTests/SwiftExtensions/Types/AutomaticLazyOptional.swift
+++ SignificantSpicesTests/SwiftExtensions/Types/AutomaticLazyOptional.swift
@@ -13,13 +13,13 @@ import SignificantSpices.Swift
 class AutomaticLazyOptionalTests: XCTestCase {
     func testGetterReturnsCorrectValue() {
         let testString: String = "Foo"
-        var lzy: AutomaticLazyOptional<String> = AutomaticLazyOptional({ return testString })
+        var lzy: ALO<String> = ALO({ return testString })
         XCTAssertEqual(lzy¡, testString)
     }
     
     func testValueCreationIsEvaluatedLazily() {
         var mustNotBeEvaluatedYet: Bool = true
-        var lzy: AutomaticLazyOptional<String> = AutomaticLazyOptional({
+        var lzy: ALO<String> = ALO({
             if mustNotBeEvaluatedYet {
                 XCTFail("The value creation closure should not have been evaluated yet")
             }
@@ -30,7 +30,7 @@ class AutomaticLazyOptionalTests: XCTestCase {
     }
     
     func testValueIsntCreatedWhenCallingOptional() {
-        let lzy: AutomaticLazyOptional<String> = AutomaticLazyOptional({
+        let lzy: ALO<String> = ALO({
             XCTFail("The value creation closure should not have been evaluated")
             return "Foo"
         })
@@ -39,7 +39,7 @@ class AutomaticLazyOptionalTests: XCTestCase {
     
     func testValueCaching() {
         var evalTimes: Int = 0
-        var lzy: AutomaticLazyOptional<String> = AutomaticLazyOptional({
+        var lzy: ALO<String> = ALO({
             evalTimes += 1
             if evalTimes > 1 {
                 XCTFail("The value creation closure should not have been evaluated more than once")
@@ -54,7 +54,7 @@ class AutomaticLazyOptionalTests: XCTestCase {
     
     func testClear() {
         class Foo {}
-        var lzy: AutomaticLazyOptional<Foo> = AutomaticLazyOptional({ Foo() })
+        var lzy: ALO<Foo> = ALO({ Foo() })
         weak var foo: Foo? = lzy¡
         
         XCTAssertNotNil(foo)
