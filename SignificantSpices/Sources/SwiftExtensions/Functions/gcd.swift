fda8b260f9e27b4b7b12334791c3f00f4e990590
Jan Nash <jan@resmio.com>
2018-05-13 18:36:10 +0200

    Nest implementation into a struct
    This makes accidentally calling the non-zero-checked implementation
    impossible

--- SignificantSpices/Sources/SwiftExtensions/Functions/gcd.swift
+++ SignificantSpices/Sources/SwiftExtensions/Functions/gcd.swift
@@ -10,31 +10,35 @@
 // MARK: // Public
 // MARK: SignedInteger Version
 public func gcd<T: SignedInteger>(_ m: T, _ n: T) -> T {
-    return _gcd(abs(m), n)
+    return _Imp.gcd(abs(m), n)
 }
 
 
 // MARK: UnsignedInteger Version
 public func gcd<T: UnsignedInteger>(_ m: T, _ n: T) -> T {
-    return _gcd(m, n)
+    return _Imp.gcd(m, n)
 }
 
 
 // MARK: // Private
-// The declaration of __gcd isn't nested inside the
-// implementation of _gcd since that messes up code coverage...
-// MARK: Sanity Checks
-private func _gcd<T: BinaryInteger>(_ m: T, _ n: T) -> T {
-    guard m != 0, n != 0 else {
-        return m + n
+// MARK: - _Imp
+private struct _Imp {
+    static func gcd<T: BinaryInteger>(_ m: T, _ n: T) -> T {
+        return self._zeroCheckedGCD(m, n)
     }
     
-    return __gcd(m, n)
-}
+    // Zero-Checked
+    private static func _zeroCheckedGCD<T: BinaryInteger>(_ m: T, _ n: T) -> T {
+        guard m != 0, n != 0 else {
+            return m + n
+        }
+        
+        return self.__gcd(m, n)
+    }
 
-
-// MARK: Actual Implementation
-private func __gcd<T: BinaryInteger>(_ m: T, _ n: T) -> T {
-    let mod: T = m % n
-    return mod == 0 ? n : __gcd(n, mod)
+    // Recursive Implementation
+    private static func __gcd<T: BinaryInteger>(_ m: T, _ n: T) -> T {
+        let mod: T = m % n
+        return mod == 0 ? n : self.__gcd(n, mod)
+    }
 }
