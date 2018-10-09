de2fca72380f4a087d2cd1968e9fdfbafc98c1cf
Jan Nash <jan@resmio.com>
2018-10-09 11:41:09 +0200

    Add Sequence forEachWhereDo extension

---
+++ SignificantSpices/Sources/SwiftExtensions/Extensions/Sequence/Sequence (forEachWhereDo).swift
@@ -0,0 +1,17 @@
+//
+//  Sequence (forEachWhereDo).swift
+//  SignificantSpices
+//
+//  Created by Jan Nash (resmio) on 09.10.18.
+//  Copyright © 2018 resmio. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+public extension Sequence {
+    public func forEach(where condition: (Element) -> Bool, `do` body: (Element) -> Void) {
+        self.forEach({ if condition($0) { body($0) } })
+    }
+}
