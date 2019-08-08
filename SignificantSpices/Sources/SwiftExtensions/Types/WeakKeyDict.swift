7811168fd52eaa4a39bed8346de177367f4e11c5
Jan Nash <jan@resmio.com>
2019-08-08 13:36:24 +0200

    Add WeakKeyDict

---
+++ SignificantSpices/Sources/SwiftExtensions/Types/WeakKeyDict.swift
@@ -0,0 +1,51 @@
+//
+//  WeakKeyDict.swift
+//  SignificantSpices
+//
+//  Created by Jan Nash (resmio) on 08.08.19.
+//  Copyright © 2019 resmio. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+// MARK: Interface
+public extension WeakKeyDict {
+    func forEach(_ body: (Key, Value) -> Void) {
+        self._forEach(body)
+    }
+    
+    subscript(key: Key) -> Value? {
+        get { return self._dict[WeakDictKeyWrapper(key: key, delegate: self)] }
+        set { self._dict[WeakDictKeyWrapper(key: key, delegate: self)] = newValue }
+    }
+}
+
+
+// MARK: Class Declaration
+public class WeakKeyDict<Key: Hashable & AnyObject, Value> {
+    // Private Variables
+    private var _dict: [WeakDictKeyWrapper<Key>: Value] = [:]
+}
+
+
+// MARK: Protocol Conformances
+// MARK: WeakDictKeyWrapperDelegate
+extension WeakKeyDict: WeakDictKeyWrapperDelegate {
+    public func disconnected<K>(wrapper: WeakDictKeyWrapper<K>) where K : AnyObject, K : Hashable {
+        self._dict[wrapper as! WeakDictKeyWrapper<Key>] = nil
+    }
+}
+
+
+// MARK: // Private
+// MARK: Interface Implementations
+private extension WeakKeyDict {
+    func _forEach(_ body: (Key, Value) -> Void) {
+        for (wrapper, value) in self._dict {
+            guard let key: Key = wrapper.key else { continue }
+            body(key, value)
+        }
+    }
+}
